b97239ca3f821bb2a0a6fdfe96ec2d4dcc9ab80d
kboosh2020 <47400371+kboosh2020@users.noreply.github.com>
2019-02-06 22:42:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('message', message => {
   var argresult = args.join(' ');
 if (message.content.startsWith(prefix + 's')) {
                                        message.delete()
-  if (message.author.id !== '358771712839385089') return;
+  if (message.author.id !== '344966841535561730') return;
 client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
     message.channel.sendMessage(`**\`${argresult}\` :تم تغيير الحالة الى**`).then(msg => {msg.delete(7000)});
 
